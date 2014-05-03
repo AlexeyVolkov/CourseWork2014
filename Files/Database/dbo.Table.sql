0a4b7adc486879cc250b0108fb762c48f1b32599
Alexey Volkov <git@ulstu.net>
2014-05-03 15:37:47 +0400

    Started database

---
+++ Files/Database/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] VARCHAR(MAX) NULL, 
+    [price] MONEY NULL, 
+    [year] INT NULL, 
+    [city] VARCHAR(MAX) NULL, 
+    [date] DATE NULL, 
+    [photo] VARCHAR(MAX) NULL, 
+    [link] VARCHAR(MAX) NULL
+)
