7c096c2fe536f8e75dd5f7cd90d984c9a857a051
Alexey Volkov <git@ulstu.net>
2014-06-05 17:02:30 +0400

    Сервер работает, нахуй!!!!!!!!!!!!!!

---
+++ Files/Server/ClientConsoleApplication/ClientConsoleApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+
+namespace Client
+{
+    [ServiceContract]
+    public interface IMyObject
+    {
+        [OperationContract]
+        string GetCommandString(int i);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri tcpUri = new Uri("http://localhost:8080/");
+            EndpointAddress address = new EndpointAddress(tcpUri);
+            BasicHttpBinding binding = new BasicHttpBinding();
+            ChannelFactory<IMyObject> factory = new ChannelFactory<IMyObject>(binding, address);
+            IMyObject service = factory.CreateChannel();
+
+            Console.WriteLine("Вызываю метод сервиса...?");
+            Console.WriteLine(service.GetCommandString(1));
+            Console.WriteLine(service.GetCommandString(2));
+            Console.WriteLine(service.GetCommandString(20));
+            Console.WriteLine(service.GetCommandString(1562492));
+            Console.WriteLine(service.GetCommandString(0));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Files/Server/DLLforServer/DLLforServer/CommunicationInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace CommunicationInterface
+{
+    [ServiceContract] // Говорим WCF что это интерфейс для запросов сервису
+    public interface IMyObject
+    {
+        [OperationContract] // Делегируемый метод.
+        string GetCommandString(int i);
+    }
+}
